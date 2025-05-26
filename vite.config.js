8397b7600dddb296c58d7ffb15159e5651e334b3
JNettli <jonas.nettli@gmail.com>
2025-05-26 13:57:03 +0200

    fix: more Safari fixes

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,25 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import legacy from "@vitejs/plugin-legacy";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), tailwindcss()],
+    plugins: [
+        react(),
+        tailwindcss(),
+        legacy({
+            targets: [
+                "defaults",
+                "not dead",
+                "Safari >= 12",
+                "iOS >= 12",
+                "Chrome >= 60",
+                "Edge >= 79",
+                "Firefox >= 60",
+            ],
+            additionalLegacyPolyfills: ["regenerator-runtime/runtime"],
+            renderLegacyChunks: true,
+            modernPolyfills: true,
+        }),
+    ],
 });
